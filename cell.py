d6843ba9cac1b1c68a8d440595bdb6cffd66cd09
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-06-18 01:12:11 +0900

    :new: UserSettingsにプロパティを定義していく

--- cell.py
+++ cell.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8 -*-
 
 from point import Point
+from userSettings import *
 
 class Cell:
     def __init__(self, canvas, id):
         self.canvas = canvas
         self.id = id
 
-        size = 20
+        size = UserSettings.cell_size()
         position = self.id * size
         
-        self.image = self.canvas.create_rectangle(position.x, position.y, position.x + size, position.y + size, fill = "blue")
+        self.image = self.canvas.create_rectangle(position.x, position.y, position.x + size, position.y + size, fill="blue")
+        
+    def remove(self):
+        self.canvas.delete(self.image)
---
+++ userSettings.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class UserSettings:
+    @staticmethod
+    def cell_size():
+        return 20
