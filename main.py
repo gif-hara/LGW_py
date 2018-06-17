4f7c5ef02dfc1d5f99a64a91f9f83ffb5f6c5e3c
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-06-17 20:52:54 +0900

    :new; お約束構文追加

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import tkinter as tk
 from cell import Cell
 from point import Point
@@ -6,8 +9,13 @@ root = tk.Tk()
 root.title("LGW")
 #root.attributes("-fullscreen", True)
 
+def input_key(event):
+    print("input = {}".format(repr(event.char)))
+
 canvas = tk.Canvas(root, bg = "#000000")
 canvas.pack(fill = tk.BOTH, expand = 1)
+canvas.bind("<Key>", input_key)
+canvas.focus_set()
 
 Cell(canvas, Point(0, 0))
 Cell(canvas, Point(1, 1))
--- point.py
+++ point.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 class Point:
     def __init__(self):
         self.x = 0
