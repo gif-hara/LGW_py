5fd4f7d0cc2df580e56126b77b1eb8963eb205f5
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-06-18 19:41:32 +0900

    :fire: 不要な要素を削除

--- cell.py
+++ cell.py
@@ -26,24 +26,7 @@ class Cell:
     def set_alive(self, isAlive):
         self.isAlive = isAlive
         self.canvas.itemconfig(self.image, state=self.get_state(self.isAlive))
-    
-    def next_generation(self, cellManager):
-        min = Point(self.id.x - 1, self.id.y - 1)
-        for y in range(3):
-            for x in range(3):
-                targetId = Point(min.x + x, min.y + y)
-                if targetId in cellManager.processed_cellIds:
-                    continue
-                adjacentNumber = cellManager.get_adjacent_number(targetId)
-                isAlive = targetId in cellManager.cellIds
-                if isAlive:
-                    if adjacentNumber <= 1 or adjacentNumber >= 4:
-                        cellManager.remove_request_ids.append(targetId)
-                else:
-                    if adjacentNumber == 3:
-                        cellManager.create_request_ids.append(targetId)
-                cellManager.processed_cellIds[targetId] = True
-    
+        
     @staticmethod
     def get_state(isAlive):
         if isAlive == True:
