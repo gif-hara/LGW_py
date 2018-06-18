50f5c62f5b9c3222bff1971bc63cacd88a65fac7
hiroki-kitahara <hk.mp5k.a4.pdw83@gmail.com>
2018-06-18 17:56:13 +0900

    :bulb: Application側にコード委譲

---
+++ application.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tkinter as tk
+from cell import Cell
+from point import Point
+from cell_manager import *
+from line_profiler import LineProfiler
+from user_input import *
+
+class Application:
+
+    _instance = None
+    
+    window = None
+
+    def __init__(self):
+        pass
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+
+    def initialize(self):
+        self._instance.window = tk.Tk()
+        self._instance.window.title("LGW")
+        self._instance.window.attributes("-fullscreen", True)
+
+        canvas = tk.Canvas(self._instance.window, bg = "#000000")
+        canvas.pack(fill = tk.BOTH, expand = 1)
+        canvas.focus_set()
+
+        cellManager = CellManager(canvas)
+        userInput = UserInput(cellManager, canvas)
+
+    def run(self):
+        self._instance.window.mainloop()
--- main.py
+++ main.py
@@ -1,46 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import tkinter as tk
-from cell import Cell
-from point import Point
-from cell_manager import *
-from line_profiler import LineProfiler
-from user_input import *
+from application import Application
 
-profiler = LineProfiler()
+app = Application()
 
-def main():
-    root = tk.Tk()
-    root.title("LGW")
-    root.attributes("-fullscreen", True)
-
-    # def input_key(event):
-    #     print("input = {}".format(repr(event.char)))
-
-    canvas = tk.Canvas(root, bg = "#000000")
-    canvas.pack(fill = tk.BOTH, expand = 1)
-    # canvas.bind("<Key>", input_key)
-    canvas.focus_set()
-
-    cellManager = CellManager(canvas)
-    userInput = UserInput(cellManager, canvas)
-    
-    # for y in range(20):
-    #     for x in range(20):
-    #         cellManager.create_cell(Point(x, y))
-    # cellManager.next_generation()
-
-    root.mainloop()
-    # root.quit()
-
-# profiler.add_module(Cell)
-# profiler.add_module(Point)
-# profiler.add_module(CellManager)
-# profiler.add_function(main)
-
-# profiler.runcall(main)
-
-# profiler.print_stats()
-
-main()
+app.initialize()
+app.run()
